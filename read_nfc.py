14aca4e5821cea6bb9198531c3fffa5e1801c0ad
정우재 <80890472+woojae05@users.noreply.github.com>
2024-08-07 18:49:43 +0900

    add file_setting

--- read_nfc.py
+++ read_nfc.py
@@ -1,10 +1,10 @@
+import ndef
 import nfc
 import usb.core
 import usb.util
 import usb.backend.libusb1
-from pylibsdm.tag.ntag424dna import Tag
-from pylibsdm.tag.ntag424dna import FileOption, SDMOptions, AccessRights, SDMAccessRights, FileSettings
-from pylibsdm.tag.ntag424dna import AccessCondition, CommMode
+from pylibsdm.tag.ntag424dna import Tag, FileSettings, SDMAccessRights, AccessCondition, FileOption, AccessRights, \
+    SDMOptions, CommMode
 
 
 def get_manufacturer_info(tag):
@@ -53,20 +53,68 @@ def on_connect(tag):
     if isinstance(tag, nfc.tag.tt4.Type4Tag):
         try:
             sdm_tag = Tag(tag)
+            print('sdm tag product:', sdm_tag.tag.product)
+            print('sdm tag type:', sdm_tag.tag.type)
+            print('sdm tag clf:', sdm_tag.tag.clf)
+            print('sdm tag is_authenticated:', sdm_tag.tag.is_authenticated)
+            print('sdm tag target:', sdm_tag.tag.target)
+            print('sdm tag identifier:', sdm_tag.tag.identifier)
 
-            # 태그 정보 확인
-            print("태그 제품 정보:", sdm_tag.get_product_info())
-            print("태그 버전 정보:", sdm_tag.get_version_info())
+            print('sdm tag ndef:', sdm_tag.tag.ndef)
+            print('sdm tag ndef tag:', sdm_tag.tag.ndef.tag)
+            print('sdm tag ndef length:', sdm_tag.tag.ndef.length)
+            print('sdm tag ndef capacity:', sdm_tag.tag.ndef.capacity)
+            print('sdm tag ndef.is_readable:', sdm_tag.tag.ndef.is_readable)
+            print('sdm tag ndef.is_writeable:', sdm_tag.tag.ndef.is_writeable)
+            print('sdm tag ndef records 0 index:', sdm_tag.tag.ndef.records[0])
 
-            # 마스터 앱 키 0 설정
-            sdm_tag.set_key(0, b"\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xaa\xbb\xcc\xdd\xee\xff")
 
-            # 앱 키 1과 2 변경
-            sdm_tag.change_key(1, 16 * b"\xaa")
-            sdm_tag.change_key(2, 16 * b"\xbb")
+            # 초기화
+            try:
+                sdm_tag.reset_session()
+                print("초기화")
+            except Exception as e:
+                print(f"초기화 실패: {e}")
 
-            # 미러링을 위한 속성 구성
-            file_option = FileOption(sdm_enabled=True, comm_mode=CommMode.PLAIN)
+            try:
+                # 초기 마스터 키 설정
+                sdm_tag.set_key(0, b"\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xaa\xbb\xcc\xdd\xee\xff")
+                print("마스터 앱 키 0 설정 성공")
+            except Exception as e:
+                print(f"마스터 앱 키 0 설정 실패: {e}")
+
+                # 앱 키 1과 2 변경
+            try:
+                # 기존 키를 새로운 값으로 변경
+                key1 = bytes.fromhex("FBC9F75C9413C041DFEE452D3F0706D1")
+                key2 = bytes.fromhex("F793EEB928278083BFDC8A5A7E0E0D25")
+
+                sdm_tag.set_key(1, key1)
+                print("앱 키 1 변경 성공")
+
+                sdm_tag.set_key(2, key2)
+                print("앱 키 2 변경 성공")
+            except Exception as e:
+                print(f"앱 키 변경 실패: {e}")
+
+            file_option = FileOption(sdm_enabled=True, comm_mode=CommMode.FULL)
+
+            access_rights = AccessRights(
+                read=AccessCondition.FREE_ACCESS,
+                write=AccessCondition.KEY_1,
+                read_write = AccessCondition.KEY_1,
+                change = AccessCondition.KEY_0,
+            )
+
+            sdm_acceess_rights = SDMAccessRights(
+                file_read=AccessCondition.KEY_2,
+                meta_read=AccessCondition.KEY_2,
+                ctr_ret=AccessCondition.KEY_2,
+            )
+
+            print('sdm_acceess_rights', sdm_acceess_rights)
+            print('file_option', file_option)
+
             sdm_options = SDMOptions(
                 uid=True,
                 read_ctr=True,
@@ -76,40 +124,25 @@ def on_connect(tag):
                 ascii_encoding=True,
             )
 
-            # 액세스 권한 설정
-            access_rights = AccessRights(
-                read=AccessCondition.FREE_ACCESS,
-                write=AccessCondition.KEY_1,
-                read_write=AccessCondition.KEY_1,
-                change=AccessCondition.KEY_0,
-            )
-            sdm_access_rights = SDMAccessRights(
-                file_read=AccessCondition.KEY_2,
-                meta_read=AccessCondition.KEY_2,
-                ctr_ret=AccessCondition.KEY_2,
-            )
-
-            # 파일 설정 구성
             file_settings = FileSettings(
                 file_option=file_option,
                 access_rights=access_rights,
                 sdm_options=sdm_options,
-                sdm_access_rights=sdm_access_rights,
+                sdm_access_rights=sdm_acceess_rights,
                 picc_data_offset=32,
-                mac_offset=67,
                 mac_input_offset=67,
+                mac_offset=131,
+                tt_status_offset=70,
+                enc_offset=67,
+                enc_length=32,
+                read_ctr_limit=1000
             )
 
-            # 파일 설정 변경
+            # print(file_data, file_data)
+            print('file 세팅 전')
             sdm_tag.change_file_settings(2, file_settings)
-
-            # NDEF 메시지 준비 및 쓰기
-            ndef_message = b"\xD1\x01\x0C\x55\x01\x6E\x74\x61\x67\x2E\x6E\x78\x70\x2E\x63\x6F\x6D\x2F\x34\x32\x34"
-            sdm_tag.update_binary(0, ndef_message)
-
-            # SUN 메시지 읽기
-            sun_message = sdm_tag.read_sdm_file()
-            print("SUN 메시지:", sun_message)
+            print('file 세팅 후')
+            # sdm_tag.tag.records = [ndef.UriRecord(file_data)]
         except Exception as e:
             print(f"SUN 기능 구현 중 오류 발생: {e}")
             print("상세 오류 정보:", str(e))
